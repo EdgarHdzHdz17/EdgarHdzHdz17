e45266f08fd182e08ebe33f0822055742781482c
Edgar Hernández Hernández <edgarhdzhdz17@hotmail.com>
2024-08-28 18:31:52 -0600

    refactor: Update internship and internship company names in Timeline section

--- src/components/sections/Timeline/index.tsx
+++ src/components/sections/Timeline/index.tsx
@@ -5,21 +5,21 @@ const Timeline = () => {
     <div className="w-full px-20">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 relative">
         <CardTimeLineComponent
-          role="Intership Frontend"
-          company="Instituto de Instituto de Ingenieria [UNAM]"
+          role="Internship Frontend"
+          company="Instituto de Ingeniería [UNAM]"
           date="Agosto 2021 - Diciembre 2021"
           description="Colaboré en la creación de sitios web utilizando HTML, CSS y
             JavaScript. Mi labor se centraba en garantizar que los sitios web
             ofrecieran una experiencia óptima en una variedad de dispositivos, asegurando así su accesibilidad y usabilidad."
         />
         <CardTimeLineComponent
-          role="Becario"
-          company="Instituto de Ciencias Aplicadas y Tecnologia [ICAT]"
+          role="Becario Frontend"
+          company="Instituto de Ciencias Aplicadas y Tecnología [ICAT]"
           date="Febrero 2022 - Diciembre 2023"
-          description="Lideré el diseño y desarrollo de sitios web utilizando Bootstrap4
-            para 'Telematica para la Educacion'. Ademas desarrolle
+          description="Lideré el diseño y desarrollo de sitios web utilizando Bootstrap 4
+            para 'Telemática para la Educación'. Además, desarrollé
             'Educatronicapp' utilizando Expo React Native como proyecto de
-            titulacion mediante tesis."
+            titulación mediante tesis."
         />
         <CardTimeLineComponent
           role="Frontend Developer"
@@ -27,8 +27,8 @@ const Timeline = () => {
           date="Junio 2024 - Actualidad"
           description="Actualmente me desempeño como Frontend Developer en Adaption, donde
             me encargo de la creación de interfaces de usuario y sistemas
-            responsivos utilizando React, tambien participo en la creacion de
-            aplicaciones moviles con React Native."
+            responsivos utilizando React. También participo en la creación de
+            aplicaciones móviles con React Native."
         />
       </div>
     </div>
